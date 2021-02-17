616a91320147ad4658f8436f4310ef82c53fc92f
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-17 11:52:44 -0700

    adds initial users index pages and components, as well as dummy data

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import UsersList from '../components/UsersList'
 
 const Users = () => {
+    const USERS = [
+        {id: 'u1', name: 'Kaelin', image: 'https://cdn.bulbagarden.net/upload/thumb/f/f4/035Clefairy.png/375px-035Clefairy.png', places: 3 },
+        {id: 'u2', name: 'Justin', image: 'https://cdn.bulbagarden.net/upload/thumb/3/3e/039Jigglypuff.png/375px-039Jigglypuff.png', places: 4},
+        {id: 'u3', name: 'Alex', image: 'https://cdn.bulbagarden.net/upload/thumb/0/02/009Blastoise.png/375px-009Blastoise.png', places: 10}
+    ]
+
     return ( 
-        <h2>Users Works!</h2>
+        <UsersList users={USERS} />
      );
 }
  
